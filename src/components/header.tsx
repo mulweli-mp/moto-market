eb74bc12ceb98941add4e34614ca4bff8699ff84
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-07-25 16:33:18 +0200

    Enabled the search function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import React, { FC, ChangeEvent } from "react";
 import styles from "./header.module.css";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,32 +8,43 @@ import {
   faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.headerContainer}>
-        <Image
-          src="/logo.png"
-          width={50}
-          height={50}
-          className={styles.logoStyle}
-          alt="motor-market logo"
-        />
-        <p className={styles.brandName}>MotoMarket</p>
+interface HeaderProps {
+  searchQuery: string;
+  handleSearch: (event: ChangeEvent<HTMLInputElement>) => void;
+}
 
-        <div className={styles.searchBox}>
-          <p>Search MotoMarket</p>
-          <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
-        </div>
-        <div className={styles.cartContainer}>
-          <FontAwesomeIcon icon={faShoppingCart} className={styles.cartIcon} />
-          <p>0</p>
-        </div>
+const Header: FC<HeaderProps> = ({ searchQuery, handleSearch }) => (
+  <div className={styles.container}>
+    <div className={styles.headerContainer}>
+      <Image
+        src="/logo.png"
+        width={50}
+        height={50}
+        className={styles.logoStyle}
+        alt="motor-market logo"
+      />
+      <p className={styles.brandName}>MotoMarket</p>
+
+      <div className={styles.searchBox}>
+        <input
+          type="text"
+          placeholder="Search MotoMarket"
+          className={styles.searchInput}
+          value={searchQuery}
+          onChange={handleSearch}
+        />
+        <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
       </div>
-      <div className={styles.sortContainer}>
-        <p>Sort by: Price</p>
-        <FontAwesomeIcon icon={faAngleDown} className={styles.arrowIcon} />
+      <div className={styles.cartContainer}>
+        <FontAwesomeIcon icon={faShoppingCart} className={styles.cartIcon} />
+        <p>0</p>
       </div>
     </div>
-  );
-}
+    <div className={styles.sortContainer}>
+      <p>Sort by: Price</p>
+      <FontAwesomeIcon icon={faAngleDown} className={styles.arrowIcon} />
+    </div>
+  </div>
+);
+
+export default Header;
