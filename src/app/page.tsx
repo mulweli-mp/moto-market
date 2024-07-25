b363bdd2c7f814b77cc05a429745ade290e3e70a
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-07-25 09:46:29 +0200

    Added components Header and BikesSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.css";
-
+import Header from "@/components/header";
+import BikesSection from "@/components/bikes-section";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>Welcome to MotorMarket</p>
+      <Header />
+      <BikesSection />
     </main>
   );
 }
