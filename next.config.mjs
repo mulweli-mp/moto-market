d9778aa9751c6096ec2beec52df4caa0fc282bfb
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-07-25 13:25:42 +0200

    Added images remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/primebet-24.appspot.com/o/MotoMarket%2F*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
