4982828acaf256fa0618d4e2c8267ff597778401
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-07-26 02:25:17 +0200

    Extracted sort feature to a seperate file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,35 +14,29 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({ searchQuery, handleSearch }) => (
-  <div className={styles.container}>
-    <div className={styles.headerContainer}>
-      <Image
-        src="/logo.png"
-        width={50}
-        height={50}
-        className={styles.logoStyle}
-        alt="motor-market logo"
-      />
-      <p className={styles.brandName}>MotoMarket</p>
+  <div className={styles.headerContainer}>
+    <Image
+      src="/logo.png"
+      width={50}
+      height={50}
+      className={styles.logoStyle}
+      alt="motor-market logo"
+    />
+    <p className={styles.brandName}>MotoMarket</p>
 
-      <div className={styles.searchBox}>
-        <input
-          type="text"
-          placeholder="Search MotoMarket"
-          className={styles.searchInput}
-          value={searchQuery}
-          onChange={handleSearch}
-        />
-        <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
-      </div>
-      <div className={styles.cartContainer}>
-        <FontAwesomeIcon icon={faShoppingCart} className={styles.cartIcon} />
-        <p>0</p>
-      </div>
+    <div className={styles.searchBox}>
+      <input
+        type="text"
+        placeholder="Search MotoMarket"
+        className={styles.searchInput}
+        value={searchQuery}
+        onChange={handleSearch}
+      />
+      <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
     </div>
-    <div className={styles.sortContainer}>
-      <p>Sort by: Price</p>
-      <FontAwesomeIcon icon={faAngleDown} className={styles.arrowIcon} />
+    <div className={styles.cartContainer}>
+      <FontAwesomeIcon icon={faShoppingCart} className={styles.cartIcon} />
+      <p>0</p>
     </div>
   </div>
 );
